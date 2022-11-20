24f20963e829da9c6b9d038bfc8d3545277c5cfe
iirene928 <iirene.928@gmail.com>
2022-11-20 20:34:41 +0100

    Second commit

--- src/Caballo.java
+++ src/Caballo.java
@@ -3,25 +3,41 @@ import java.util.logging.Logger;
 
 public class Caballo implements Runnable {
 	private String name;
-	private int kilometros;
+	private int segundos;
 	
-	Caballo (String name, int kilometros){
+	Caballo (String name, int segundos){
 		this.name=name;
-		this.kilometros=kilometros;
+		this.segundos=segundos;
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		try {
-			Thread.sleep(kilometros*1000);
-			System.out.println("El " + name + " ha recorrido "+ kilometros + " kilometros." );
+			Thread.sleep(segundos*1000);
+			System.out.println("El " + name + " ha tardado "+ segundos + " segundos." );
 			
 		}
 		catch (InterruptedException ex) {
 			Logger.getLogger(Caballo.class.getName()).log(Level.SEVERE, null, ex);
 			}
 		}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSegundos() {
+		return segundos;
+	}
+
+	public void setSegundos(int segundos) {
+		this.segundos = segundos;
+	}
 		
 	}
 
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,23 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
+	
+	private static Caballo caballoGanador(Caballo c1, Caballo c2) {
+		if (c1.getSegundos() <= c2.getSegundos()) {
+			 return c1;
+		}
+		else {
+			return c2;
+		}
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		int randomNum = ThreadLocalRandom.current().nextInt(0, 10 + 1);
+		int randomNum1 = ThreadLocalRandom.current().nextInt(0, 10 + 1);
+		int randomNum2 = ThreadLocalRandom.current().nextInt(0, 10 + 1);
+		Caballo ganador;
+		
 		Scanner in = new Scanner (System.in);
 		
 		System.out.println("Introduce el nombre del caballo 1:");
@@ -19,10 +32,8 @@ public class Main {
 
 		
 		Caballo caballo1 = new Caballo (in1,randomNum);
-		randomNum = ThreadLocalRandom.current().nextInt(0, 10 + 1);
-		Caballo caballo2 = new Caballo (in2, randomNum);
-		randomNum = ThreadLocalRandom.current().nextInt(0, 10 + 1);
-		Caballo caballo3 = new Caballo (in3, randomNum);
+		Caballo caballo2 = new Caballo (in2, randomNum1);
+		Caballo caballo3 = new Caballo (in3, randomNum2);
 		
 		Thread t= new Thread (caballo1);
 		System.out.println("Sale el caballo " + in1);
@@ -35,6 +46,17 @@ public class Main {
 		Thread t3= new Thread (caballo3);
 		System.out.println("Sale el caballo " + in3);
 			t3.start();
+			
+		t.join();
+		t2.join();
+		t3.join();
+		
+		ganador = caballoGanador(caballo1, caballo2);
+		ganador = caballoGanador(ganador, caballo3);
+		
+		
+		System.out.println("El caballo ganador es " + ganador.getName());
+		System.out.println("En total han tardado " + (caballo1.getSegundos() + caballo2.getSegundos() + caballo3.getSegundos()) + " segundos.");	
 		
 	}
 
